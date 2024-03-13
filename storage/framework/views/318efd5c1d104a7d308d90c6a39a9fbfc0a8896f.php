4ef9add5ffb0b5379ec1338c65d3cf656df78fd8
Hiran Kevin <117870293+HiranKDS@users.noreply.github.com>
2024-03-12 21:30:52 -0300

    Add files via upload

---
+++ storage/framework/views/318efd5c1d104a7d308d90c6a39a9fbfc0a8896f.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Exemplo Aula 1</title>
+    </head>
+    <body>
+        <h1>Exemplo</h1>
+        <p>Olá Mundo</p>
+    </body>
+</html>
+<?php /**PATH G:\Salvar Documentos\Aula 1\aula1\resources\views/exemplo.blade.php ENDPATH**/ ?>
